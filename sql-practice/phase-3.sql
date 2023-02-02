b8843bf43c8914e5c2c1d77370d20cd2e8351f95
steven cornwall <taylorchangordie@icloud.com>
2023-02-01 18:12:07 -0600

    phase 03

--- sql-practice/phase-3.sql
+++ sql-practice/phase-3.sql
@@ -1,1 +1,17 @@
 -- Your code here
+
+-- 1. insert a new customer
+INSERT INTO customers (name, phone)
+VALUES ('rachel', '1111111111');
+
+UPDATE customers SET points = points+1
+WHERE id = 1;
+
+INSERT INTO coffee_orders (is_redeemed) VALUES (0);
+-- INSERT INTO coffee_orders DEFAULT VALUES
+
+INSERT INTO customers (name, email, phone)
+VALUES
+('monica', 'monica@friends.show', '2222222222'),
+('phoebe', 'phoebe@friends.show', '3333333333');
+
